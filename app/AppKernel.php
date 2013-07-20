59e5d33f87831d1f093e4123c8d6687254bf2e53
adelinebechade <adelinebechade@gmail.com>
2013-07-20 17:02:45 +0200

    Création du bundle CompteBundle en annotation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Banko\CompteBundle\BankoCompteBundle(),
+            new Bundle\CompteBundle\BundleCompteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Bundle/CompteBundle/BundleCompteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bundle\CompteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BundleCompteBundle extends Bundle
+{
+}
